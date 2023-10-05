1414a7ca2d85f453fa8934dd682e8c9f0f8c1bdd
Nikunj <46291594+Nikunj47@users.noreply.github.com>
2023-10-05 17:06:27 -0400

    Updated the data for different threads

--- monitor.py
+++ monitor.py
@@ -1,23 +1,16 @@
 from sdcclient import IbmAuthHelper, SdMonitorClient
 import sys
-# Add the monitoring instance information that is required for authentication
+
+
 URL = "https://ca-tor.monitoring.cloud.ibm.com"
 APIKEY = "ryGqL7Thrn0EvtVjKsAGAXcyPNtPNlFXFDwF4T_Pz-Tk"
 GUID = "b92c514a-ca21-4548-b3f0-4d6391bab407"
 ibm_headers = IbmAuthHelper.get_headers(URL, APIKEY, GUID)
 
-# Instantiate the Python client
+
 sdclient = SdMonitorClient(sdc_url=URL, custom_headers=ibm_headers)
 
-# Fetch the list of available metrics
-# ok, res = sdclient.get_metrics()
-# if not ok:
-#     print(f"Error fetching metrics: {res}")
-#     sys.exit(1)
-# metrics = [{'jmx_jvm_class_loaded'}]
-# metrics = [
-#     {"id": "jmx_jvm_class_loaded", "aggregations": {"time": "timeAvg", "group": "avg"}}
-# ]
+
 metrics = [
     {"id": "jmx_jvm_class_loaded", "aggregations": {"time": "timeAvg", "group": "avg"}},
     {"id": "jmx_jvm_class_unloaded", "aggregations": {"time": "timeAvg", "group": "avg"}},
@@ -42,12 +35,10 @@ metrics = [
     {"id": "sysdig_container_cpu_cores_used_percent", "aggregations": {"time": "timeAvg", "group": "avg"}},
     {"id": "sysdig_container_memory_used_percent", "aggregations": {"time": "timeAvg", "group": "avg"}}
 ]
-start = -600
+start = -10
 end = 0
 
-# Sampling time:
-#   - for time series: sampling is equal to the "width" of each data point (expressed in seconds)
-#   - for aggregated data (similar to bar charts, pie charts, tables, etc.): sampling is equal to 0
+
 sampling = 60
 ok, res = sdclient.get_data(metrics,start,end)
 if not ok:
@@ -57,24 +48,3 @@ if not ok:
 
 for i in range(0, len(metrics)):
     print(str(metrics[i]['id']) + ': ' + str(res['data'][0]['d'][i]))
-
-# # Filter out JMX metrics
-# jmx_metrics = [metric['name'] for metric in res['metrics'] if 'jmx' in metric['name']]
-
-# # Create a list of metric dictionaries for the get_data method
-# metrics_to_fetch = [{"id": metric_name, "aggregations": {"time": "timeAvg", "group": "avg"}} for metric_name in jmx_metrics]
-
-# # Time window and sampling settings
-# start = -600
-# end = 0
-# sampling = 60
-# filter = None
-
-# # Fetch data for JMX metrics
-# for metric in metrics_to_fetch:
-#     ok, metric_data = sdclient.get_data([metric], start, end, sampling, filter=filter)
-#     if ok:
-#         print(metric_data)
-#     else:
-#         print(f"Error fetching data for metric {metric['id']}: {metric_data}")
-
