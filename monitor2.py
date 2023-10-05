73b9efe5415c46b04634e2dbf78f5c889d17f55d
Nikunj <46291594+Nikunj47@users.noreply.github.com>
2023-10-04 22:06:08 -0400

    Update monitor2.py

--- monitor2.py
+++ monitor2.py
@@ -1,64 +1,72 @@
 from sdcclient import IbmAuthHelper, SdMonitorClient
 import sys
-# Add the monitoring instance information that is required for authentication
+import time
+import matplotlib.pyplot as plt
+
 URL = "https://ca-tor.monitoring.cloud.ibm.com"
 APIKEY = "ryGqL7Thrn0EvtVjKsAGAXcyPNtPNlFXFDwF4T_Pz-Tk"
 GUID = "b92c514a-ca21-4548-b3f0-4d6391bab407"
 ibm_headers = IbmAuthHelper.get_headers(URL, APIKEY, GUID)
 
-# Instantiate the Python client
 sdclient = SdMonitorClient(sdc_url=URL, custom_headers=ibm_headers)
 
-# Fetch the list of available metrics
-# ok, res = sdclient.get_metrics()
-# if not ok:
-#     print(f"Error fetching metrics: {res}")
-#     sys.exit(1)
-# metrics = [{'jmx_jvm_class_loaded'}]
-# metrics = [
-#     {"id": "jmx_jvm_class_loaded", "aggregations": {"time": "timeAvg", "group": "avg"}}
-# ]
 metrics = [
     {"id": "jmx_jvm_class_loaded", "aggregations": {"time": "timeAvg", "group": "avg"}},
     {"id": "jmx_jvm_class_unloaded", "aggregations": {"time": "timeAvg", "group": "avg"}},
     {"id": "jmx_jvm_gc_global_count", "aggregations": {"time": "timeAvg", "group": "avg"}},
     {"id": "jmx_jvm_gc_global_time", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_gc_scavenge_count", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_gc_scavenge_time", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_heap_committed", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_heap_init", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_heap_max", "aggregations": {"time": "timeAvg", "group": "avg"}},
     {"id": "jmx_jvm_heap_used", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_heap_used_percent", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_nonHeap_committed", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_nonHeap_init", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_nonHeap_max", "aggregations": {"time": "timeAvg", "group": "avg"}},
     {"id": "jmx_jvm_nonHeap_used", "aggregations": {"time": "timeAvg", "group": "avg"}},
-    {"id": "jmx_jvm_thread_count", "aggregations": {"time": "timeAvg", "group": "avg"}}
+    {"id": "jmx_jvm_nonHeap_used_percent", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_thread_count", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "jmx_jvm_thread_daemon", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "sysdig_connection_net_request_time", "aggregations": {"time": "timeAvg", "group": "max"}},
+    {"id": "sysdig_connection_net_connection_total_count", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "sysdig_container_cpu_cores_used_percent", "aggregations": {"time": "timeAvg", "group": "avg"}},
+    {"id": "sysdig_container_memory_used_percent", "aggregations": {"time": "timeAvg", "group": "avg"}}
 ]
-start = -600
-end = 0
 
-# Sampling time:
-#   - for time series: sampling is equal to the "width" of each data point (expressed in seconds)
-#   - for aggregated data (similar to bar charts, pie charts, tables, etc.): sampling is equal to 0
-sampling = 60
-ok, res = sdclient.get_data(metrics,start,end)
-if not ok:
-    print(f"Error fetching metrics: {res}")
-    sys.exit(1)
+interval = 3
 
+# Store data
+data_storage = {metric['id']: [] for metric in metrics}
 
-print(res)
+# Use a timer to fetch data for exactly 10 seconds
+end_time = time.time() + 60
 
-# # Filter out JMX metrics
-# jmx_metrics = [metric['name'] for metric in res['metrics'] if 'jmx' in metric['name']]
+while time.time() < end_time:
+    start = -10
+    end = 0
+    # sampling = 60
+    ok, res = sdclient.get_data(metrics, start, end,filter = "kube_cluster_name='ece750cluster' and kube_namespace_name='acmeair-g11' and kube_workload_name ='acmeair-bookingservice'")
+    if not ok:
+        print(f"Error fetching metrics: {res}")
+        sys.exit(1)
+    
+    for metric in metrics:
+        metric_id = metric['id']
+        data_storage[metric_id].append(res['data'][0]['d'][metrics.index(metric)])
+    
+    # Sleep for the interval duration
+    time.sleep(interval)
 
-# # Create a list of metric dictionaries for the get_data method
-# metrics_to_fetch = [{"id": metric_name, "aggregations": {"time": "timeAvg", "group": "avg"}} for metric_name in jmx_metrics]
-
-# # Time window and sampling settings
-# start = -600
-# end = 0
-# sampling = 60
-# filter = None
-
-# # Fetch data for JMX metrics
-# for metric in metrics_to_fetch:
-#     ok, metric_data = sdclient.get_data([metric], start, end, sampling, filter=filter)
-#     if ok:
-#         print(metric_data)
-#     else:
-#         print(f"Error fetching data for metric {metric['id']}: {metric_data}")
-
+# Plotting each metric in its own figure
+for metric_id, values in data_storage.items():
+    # print(f"Data for {metric_id}: {values}")  # Print the collected data
+    
+    plt.figure(figsize=(10, 5))  # Increase the figure size for better resolution
+    plt.plot(values, marker='o', linestyle='-')  # Use markers to visualize individual data points
+    plt.title(metric_id)
+    plt.xlabel('Time Intervals')
+    plt.ylabel('Metric Value')
+    plt.grid(True)  # Add a grid for better visualization
+    plt.show()
